dc3d1de97c8773ca4be19fbcb9f3707b47b8be74
Anvesh chidura <31155837+anveshchidura@users.noreply.github.com>
2024-04-13 02:35:37 -0400

    modified env variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,14 +4,13 @@ export const LOGO =
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNGZlMWI1MWI3OTFiNzljMDM1Nzc1MDc0NmMxMmVlYyIsInN1YiI6IjY1NGMwODM3ZmQ0ZjgwMDEwMWI4NDQzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6Bfr0D5uVEDFgkST2V2AN5XSPnORmREUMXPSXMn3gLo"
-    ,
-  },
-};
+  export const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+    },
+  };
     
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 export const SEARCH_MOVIES = "https://api.themoviedb.org/3/search/movie?query=";
